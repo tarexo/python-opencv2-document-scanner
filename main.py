1013dcd40430ea0caa50b9736cc76be6ce965e1a
tarexo <koschinski.y@gmail.com>
2022-05-10 20:40:41 +0200

    Add comments and improve visualization

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import numpy as np
 import math
 import os
 
+# Change the image base path if needed and apply settings as needed
 img_base_path = "images/"
 scaling_factor = 0.25
-visualize = True
-save = False
+visualize = True            # Wether to visualize the process
+save = False                # Wether to safe the result to the img_base_path directory
 
 def main():
     images = os.listdir("images")
@@ -14,6 +15,7 @@ def main():
         if file == ".gitignore": 
             continue
 
+        # Load the image and save the dimensions. The height is used for defining the height of the scanned image
         original_img = cv2.imread(img_base_path + file)
         org_max_y, org_max_x, _ = original_img.shape
         target_height = org_max_y
@@ -26,17 +28,21 @@ def main():
         paper_corners = findPaperCorners(corners, img)
         paper_corners = resetScaling(paper_corners)
 
+        # Find the homography matrix and use it to transform the image to a document scan
         M, _ = cv2.findHomography(np.array(list(paper_corners)), np.array([[0,0], [int(target_height / math.sqrt(2)), 0], [0, target_height], [int(target_height / math.sqrt(2)), target_height]]))
         img_warped = cv2.warpPerspective(original_img, M, (int(target_height / math.sqrt(2)), target_height))
 
+        # Transform the BGR image to a binary image
         img_warped = applyThreshold(img_warped)
 
+        # Concatenate the original image and the result for easy comparison
         combination = cv2.hconcat([original_img, img_warped])
         combination = resizeImage(combination, factor=0.4)
 
         if visualize:
             cv2.imshow("WARPED DOCUMENT", combination)
             cv2.waitKey(0)
+            cv2.destroyAllWindows()
         
         if save:
             cv2.imwrite(img_base_path + "combined_" + file, combination)
@@ -44,13 +50,37 @@ def main():
 
 
 def preprocessing(img):
+    """Preprocesses a given image.
+
+    Args:
+        img (numpy.ndarray): The image to preprocess
+
+    Returns:
+        numpy.ndarray: The preprocessed image
+    """
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = resizeImage(img)
     img = cv2.GaussianBlur(img, (3,3), 0)
+
+    if visualize:
+        cv2.imshow("IMAGE AFTER PREPROCESSING", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     return img
 
 
 def resizeImage(img, factor=scaling_factor):    
+    """Resized a given image by a factor.
+
+    Args:
+        img (numpy.ndarray): The image to be resized
+        factor (float, optional): The factor to multiply the height and width of the image with. Defaults to scaling_factor
+
+    Returns:
+        numpy.ndarray: The resized image
+    """
+
     w = int(img.shape[1] * factor)
     h = int(img.shape[0] * factor)
     dims = (w, h)
@@ -58,17 +88,37 @@ def resizeImage(img, factor=scaling_factor):
 
 
 def fastCornerDetection(img):
+    """Runs the FAST corner detection on an image.
+
+    Args:
+        img (numpy.ndarray): The image on which corners shall be detected
+
+    Returns:
+        (cv2.KeyPoint, cv2.KeyPoint, ...): The detected corners 
+    """
+
     fast = cv2.FastFeatureDetector_create()
     kp = fast.detect(img, None)
 
     if visualize:
         img = cv2.drawKeypoints(img, kp, None, color=(0,255,0))
         cv2.imshow("FAST CORNER DETECTION", img)
-        cv2.waitKey(1)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     return kp 
 
 
 def findPaperCorners(detections, img):
+    """Find the four corners of the document visible on the iamge.
+
+    Args:
+        detections (numpy.ndarray): All the corners detected by a detection algorithm like FAST
+        img (numpy.ndarray): The image 
+
+    Returns:
+        tuple: The coordinates of the four corner points (top left, top right, bottom left, bottom right)
+    """
+
     cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
     (max_y, max_x) = img.shape
@@ -104,12 +154,25 @@ def findPaperCorners(detections, img):
             canvas = cv2.circle(canvas, (bl[0], bl[1]), radius=3, color=(0,255,0), thickness=-1)
             canvas = cv2.circle(canvas, (br[0], br[1]), radius=3, color=(0,255,0), thickness=-1)
             cv2.imshow("FIND PAPER CORNERS", canvas)
-            cv2.waitKey(20)
+            cv2.waitKey(50)
+    
+    if visualize:
+        cv2.destroyAllWindows()
 
     return (tl, tr, bl, br)
 
 
 def resetScaling(paper_corners):
+    """Reset the scaling done by the preprocessing to get the coordinates of the paper corners on the original image resolution.
+
+    Args:
+        paper_corners (tuple): The coordinates of the paper corners
+
+    Returns:
+        tuple: The rescaled coordinates of the paper corners
+    """
+
+    print(type(paper_corners))
     for corner in paper_corners:
         corner[0] = corner[0] / scaling_factor
         corner[1] = corner[1] / scaling_factor
@@ -117,6 +180,15 @@ def resetScaling(paper_corners):
 
 
 def applyThreshold(img):
+    """Applies a adaptive threshold on a given image.
+
+    Args:
+        img (numpy.ndarray): The image
+
+    Returns:
+        numpy.ndarray: The binary image
+    """
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 10)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
